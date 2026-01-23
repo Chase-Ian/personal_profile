2136a20d13d4b65cd598ae6a595012dff8314c4c
enderplayz05 <chaseian05@gmail.com>
2026-01-23 12:43:53 +0800

    adding the bones of navigation

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,118 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,                // just removes the debug banner
+      home: MyHomePage(title: 'Personal Profile')
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  
+  const MyHomePage({super.key, required this.title});
+  
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //header
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  color: Colors.green,
+                ),
+
+                // Avatar
+                Positioned(
+                  top: 50,
+                  child: CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.person,
+                      size: 50,
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+
+                // Name
+                const Positioned(
+                  bottom: 20,
+                  child: Text(
+                    "Your name",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                  Positioned(
+                    top: 95,
+                    left: MediaQuery.of(context).size.width / 2 + 20,
+                    child: CircleAvatar(
+                      radius: 14,
+                      backgroundColor: Colors.blue,
+                      child: Icon(
+                        Icons.camera_alt,
+                        size: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            Center(
+            child:ElevatedButton(
+              child: const Text('Open route'),
+                onPressed: () {
+                Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                builder: (context) => const SecondRoute(),
+                    ),
+                   );
+                  },
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Route'), automaticallyImplyLeading: false,), //NOTE automaticallyImplyLeading is just removing the automatic back button when using navigator.push
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
         ),
       ),
     );
